9cb41fa7dee447c01bcc1f9ae437ceec24a6b91c
Guy Moore <guy.moore.normal@gmail.com>
2017-08-18 15:09:43 -0700

    Add parsing to log contents
    parse based on each field in the slo police blotter

--- main.py
+++ main.py
@@ -23,12 +23,37 @@ client = MongoClient('localhost', 27017)
 db = client.test
 logs = db.police_logs
 
+type_pattern = re.compile("Type: (\S*)\s")
+location_pattern = re.compile("Location:(\S*)\s")
+address_pattern = re.compile("Addr: (\S*)\s")
+clearance_code_pattern = re.compile("Clearance Code: (\S*)\s")
+responsible_officer_pattern = re.compile("Responsible Officer: (\S*)\s")
+call_comments_pattern = re.compile("CALL_COMMENTS: (\S*)\s")
+
 for line in lines:
-    pattern = re.compile("Type: (.*)\s")
-    match = pattern.match(line)
+    entry = {"raw":line}
+    match = type_pattern.match(line)
     if match:
-        entry_type = match.group(0)
-        print(entry_type)
-    entry_location = re.findall("Location:(.*)\n$", line)
-    entry = {"raw":line, "type": entry_type, "location": entry_location}
-    #logs.insert_one(entry)
+        entry["type"] = match.group(1)
+        print(entry["type"])
+    match = location_pattern.match(line)
+    if match:
+        entry["location"] = match.group(1)
+        print(entry["location"])
+    match = address_pattern.match(line)
+    if match:
+        entry["address"] = match.group(1)
+        print(entry["address"])
+    match = clearance_code_pattern.match(line)
+    if match:
+        entry["clearance_code"] = match.group(1)
+        print(entry["clearance_code"])
+    match = responsible_officer_pattern.match(line)
+    if match:
+        entry["responsible_officer"] = match.group(1)
+        print(entry["responsible_officer"])
+    match = call_comments_pattern.match(line)
+    if match:
+        entry["call_comments"] = match.group(1)
+        print(entry["call_comments"])
+    logs.insert_one(entry)
