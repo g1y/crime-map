24d11c90eee40f11d1fcbcb2632bf44f52543810
Guy Moore <guy.moore.normal@gmail.com>
2017-08-19 22:54:06 -0700

    Grab both header and body
    The entries are split into two parts, a header with the time and
    information about when things happened and then a body which has all the
    details. Both are handy, but splitting based on the header separator is
    tricky, so the split splits the header and body into two different
    pices, and then they need to be recombined later.

--- main.py
+++ main.py
@@ -13,25 +13,27 @@ c.close()
 
 body = buffer.getvalue()
 
-separator = '={79}\s\n.*\n.*\n'
+separator = '={79}\n'
 expression = separator
 lines = re.split(expression, body)
-lines = lines[1:]
-lines = lines[:len(lines)]
+print(lines)
+#lines = lines[1:]
+#lines = lines[:len(lines)]
 
 client = MongoClient('localhost', 27017)
 db = client.test
 logs = db.police_logs
 
-type_pattern = re.compile("Type: (\S*)\s")
-location_pattern = re.compile("Location:(\S*)")
-address_pattern = re.compile("Addr: (.*),")
-clearance_code_pattern = re.compile("Clearance Code: (\S*)\s")
-responsible_officer_pattern = re.compile("Responsible Officer: (\S*, .)")
-call_comments_pattern = re.compile("CALL COMMENTS: (.*)\n")
-
-for line in lines:
+def parse_entry(line):
     entry = {"raw":line}
+
+    type_pattern = re.compile("Type: (\S*)\s")
+    location_pattern = re.compile("Location:(\S*)")
+    address_pattern = re.compile("Addr: (.*),")
+    clearance_code_pattern = re.compile("Clearance Code: (\S*)\s")
+    responsible_officer_pattern = re.compile("Responsible Officer: (\S*, .)")
+    call_comments_pattern = re.compile("CALL COMMENTS: (.*)\n")
+
     match = type_pattern.match(line)
     if match:
         entry["type"] = match.group(1)
@@ -50,4 +52,24 @@ for line in lines:
     search = call_comments_pattern.search(line)
     if search:
         entry["call_comments"] = search.group(1)
-    logs.insert_one(entry)
+
+    return entry
+
+# It's hard to split on every nth occurence so splitting up the header and
+# body and then recombining is how I decided to shortcut that.
+def combine_header_body(lines):
+    combined_lines = []
+    current_entry = ""
+    for line in lines:
+        body_pattern = re.compile("Type: (\S*)\s")
+        header_pattern = re.compile(".*Received:.*Dispatched:")
+        body_match = body_pattern.match(line)
+        header_match = header_pattern.match(line)
+        if body_match:
+            current_entry = line
+        elif header_match:
+            current_entry = current_entry + line
+
+    return combined_lines
+
+body = parse_entry(line)
