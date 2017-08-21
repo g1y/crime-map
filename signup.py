9dc21fcc2fae7f8af5c616980ba7d273ebaaa923
Guy Moore <guy.moore.normal@gmail.com>
2017-08-21 12:34:21 -0700

    Add signup endpoint
    Takes in just the street and email and throws it in the db. Confirm is
    false since the email address needs to be confirmed as valid and owned
    by the person who submitted it.

--- signup.py
+++ signup.py
@@ -1,14 +1,28 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    email = request.form['email']
+    street = request.form['street']
+
+    signup_entry = {'email': email, 'street': street, 'confirmed': false}
+    signup_db = get_db()
+    if not signup_db.find_one(signup_entry):
+        signup_db.insert_one(signup_entry)
+
+    return "signup complete!"
 
 @app.route('/')
 def main_page():
     return render_template('signup.html')
 
-@app.route('/signup', methods=['POST'])
-def signup():
-    email = request.form['email']
-    address = request.form['street']
-    return email + address
+def get_db():
+    client = MongoClient('localhost', 27017)
+    db = client.snoopy
+    return db.signups
