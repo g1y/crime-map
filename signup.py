b4e22e845aa41fbd0c39bdb31016ff93fada7945
Guy Moore <guy.moore.normal@gmail.com>
2017-08-21 13:27:04 -0700

    Create empty celery task to send confirmation email

--- signup.py
+++ signup.py
@@ -2,9 +2,17 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+from celery import Celery
+
 from pymongo import MongoClient
 
 app = Flask(__name__)
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
+
+celery = make_celery(flask_app)
 
 @app.route('/signup', methods=['POST'])
 def signup():
@@ -16,6 +24,8 @@ def signup():
     if not signup_db.find_one(signup_entry):
         signup_db.insert_one(signup_entry)
 
+    send_confirmation_email()
+
     return "signup complete!"
 
 @app.route('/')
@@ -26,3 +36,21 @@ def get_db():
     client = MongoClient('localhost', 27017)
     db = client.snoopy
     return db.signups
+
+def make_celery(app):
+    celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+@celery.task()
+def send_confirmation_email(email):
+    # need to implement email sending
+    return
