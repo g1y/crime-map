3f237e72d2ee2dc7f9918d5ade062c817aeb31ba
Guy Moore <guy.moore.normal@gmail.com>
2017-09-10 23:15:00 -0400

    Reparsing the address field
    Go through each entry and reparse, but only update the address entry
    since that one was the most screwed up.

---
+++ reparse_slopd_logs.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+import slopd_log_parse
+import pprint
+
+#from numpy import numpy
+def reparse_entry(existing_entry):
+	reparsed = slopd_log_parse.parse_entry(existing_entry['raw'])
+	reparsed['_id'] = existing_entry['_id']
+	return reparsed
+
+client = MongoClient('localhost', 27017)
+db = client.snoopy
+logs = db.police_logs
+
+lookup = {}
+entries = map(reparse_entry, logs.find(lookup))
+for entry in entries:
+	existing = logs.update({'_id': entry['_id']}, {'$set': {'address': entry['address']}})
