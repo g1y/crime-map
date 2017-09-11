8c2acd49db7d82f94969f87d9c7622d40ca2a641
Guy Moore <guy.moore.normal@gmail.com>
2017-09-10 20:15:36 -0400

    Add geocoding for addresses in db
    Find any entries that don't have an entry for maps_geocode but
    do have an address, then iterate over each one, geocoding and
    storing it back in the database.

--- location.py
+++ location.py
@@ -1,14 +1,26 @@
 from pymongo import MongoClient
+from googlemaps import googlemaps
+
+import re
 
 key_file = open('maps_key.txt')
-maps_key = key_file.readline()
-assert key_file, 'Maps API key required to run!'
+maps_key = re.sub('\n', '', key_file.readline())
+assert maps_key, 'Maps API key required to run!'
 
 client = MongoClient('localhost', 27017)
 db = client.snoopy
 logs = db.police_logs
 
-missing_lookup = {'maps_lookup': {'$exists': False}}
+gmaps_client = googlemaps.Client(key=maps_key)
+
+missing_lookup = {'maps_geocode': {'$exists': False}, 'address': {'$exists': True}}
 log_entries = logs.find(missing_lookup)
-for entry in log_entries:
-	print(entry)
+for key, entry in enumerate(log_entries):
+	print(entry[u'address'])
+	geocode_result = gmaps_client.geocode(entry[u'address'])
+	print(geocode_result)
+	if geocode_result:
+		log_entries[key].maps_lookup = geocode_result
+		print("going to update")
+		print(geocode_result)
+		logs.update({'_id': entry._id}, {'$set': {'maps_geocode': geocode_result}}, {'upsert': True})
