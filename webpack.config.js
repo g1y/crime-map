b34d4b5923df606a4b85e1f2e11e980c1a33d96a
Guy Moore <guy.moore.normal@gmail.com>
2017-09-15 00:48:14 -0400

    Add webpack config and entrypoint

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./static/entry.js", // string | object | array
+  // Here the application starts executing
+  // and webpack starts bundling
+
+  output: {
+    // options related to how webpack emits results
+
+    path: path.resolve(__dirname, "dist"), // string
+    // the target directory for all output files
+    // must be an absolute path (use the Node.js path module)
+
+    filename: "bundle.js", // string
+    // the filename template for entry chunks
+
+    //publicPath: "/assets/", // string
+    // the url to the output directory resolved relative to the HTML page
+
+    //library: "MyLibrary", // string,
+    // the name of the exported library
+
+    //libraryTarget: "umd", // universal module definition
+    // the type of the exported library
+  },
+}
