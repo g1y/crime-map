70292d34f74a2cad83f7a951321c2b1909fac5b0
Guy Moore <guy.moore.normal@gmail.com>
2018-02-28 02:59:41 -0500

    Remove celery tasks since they weren't being used

--- signup.py
+++ signup.py
@@ -3,12 +3,7 @@ from flask import render_template
 from flask import request
 from flask import send_file
 
-from celery import Celery
-
 from pymongo import MongoClient
-
-from flask_mail import Mail
-from flask_mail import Message
 
 import json
 from bson import json_util
@@ -18,38 +13,7 @@ import os
 
 import time
 
-def make_celery(app):
-    celery = Celery(app.import_name, backend=app.config['CELERY_RESULT_BACKEND'],
-        broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
-    class ContextTask(TaskBase):
-        abstract = True
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
-
 app = Flask(__name__)
-app.config.update(
-    CELERY_BROKER_URL='redis://localhost:6379',
-    CELERY_RESULT_BACKEND='redis://localhost:6379'
-)
-
-
-celery = make_celery(app)
-
-@celery.task()
-def send_confirmation_email(email):
-    token = create_confirmation_token(email)
-    mail = Mail()
-    mail.init_app(app)
-    link = 'mooreguy.com/confirm?email=' + email + '&token=' + token
-    anchor = '<a href="' + link + '">Confirm here!</a>'
-    msg = Message(anchor, sender="noreply@mooreguy.com", recipients=email)
-    mail.send(msg)
-    return
 
 @app.route('/signup', methods=['POST'])
 def signup():
@@ -120,10 +84,6 @@ def get_logs_db():
 		logs = db.police_logs
 		return logs
 
-def create_confirmation_token(email):
-    token = os.urandom(20)
-    client = MongoClient('localhost', 27017)
-    signups = get_db()
-    signups.update({'email': email}, {'$set': {'confirmation_token': Binary(token)}}, upsert=False)
-    return token
 
+if __name__ == "__main__":
+    app.run()
