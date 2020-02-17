16588f35319531ecaf5ed78a8df73a384826d24c
Guy Moore <guy@g1y.io>
2020-02-17 01:05:29 -0800

    Send new entries to Kafka to be checked by the sighted pipeline
    The sighted pipeline needs to see all new entries coming in, to check
    whether or not the entry was sighted by someone.

--- log-scraper/main.py
+++ log-scraper/main.py
@@ -1,9 +1,13 @@
 import sentry_sdk
 sentry_sdk.init("https://1ffbb5ff89b440338e3112310ac30750@sentry.io/1864498")
 
-import pycurl
 import io
+import json
+
+import pycurl
 from pymongo import MongoClient
+from kafka import KafkaProducer
+
 
 import slopd_log_parse
 
@@ -24,11 +28,21 @@ client = MongoClient('mongodb')
 db = client.snoopy
 logs = db.police_logs
 
+producer = KafkaProducer(bootstrap_servers='kafka:9092',
+		value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
 totalInserted = 0
+totalEntries = 0
 for entry in parsed:
+	totalEntries += 1
 	existing = logs.find_one({"report_number": entry["report_number"]})
 	if not existing:
-		logs.insert_one(entry)
+		id = logs.insert_one(entry).inserted_id
+		try:
+			producer.send('log-entries', {'id': id})
+		except Exception as e:
+			sentry_sdk.capture_exception(e)
+
 		totalInserted = totalInserted + 1
 
-print("finished scraping " +  str(totalInserted) + "/" + len(parsed) + " logs")
+print("finished scraping " +  str(totalInserted) + "/" + str(totalEntries) + " logs")
