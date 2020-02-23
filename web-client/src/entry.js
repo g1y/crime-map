5441ec20de73aa37cb146e2aa2b3518406e9b25b
Guy Moore <guy@g1y.io>
2020-02-23 00:44:50 -0800

    Remove unused crime categories page and duplicate file

--- web-client/src/entry.js
+++ web-client/src/entry.js
@@ -4,30 +4,6 @@ import Table from 'react-bootstrap/Table';
 
 import App from './app';
 
-
-function addTable() {
-	fetch('/categories').then(function(response) {
-		if (response.status != '200') {
-			var error = "There was an issue with retrieving table data";
-			console.error(error, response);
-			throw error
-		}
-		return response.json();
-	}).then(function(responseJson) {
-		var table = <Table>
-			<thead>
-				<tr>
-					<th>Type</th>
-				</tr>
-			</thead>
-			<tbody>
-				{responseJson.map((type) => <tr><td>{type}</td></tr>)}
-			</tbody>
-		</Table>;
-		ReactDOM.render(table, document.getElementById("header"));
-	});
-}
-
 console.log(__API_ROOT__)
 
 ReactDOM.render(<App/>, document.getElementById("app"));
