00c88ec0b674d9cd687081f48231bddfd4ccba6d
Guy Moore <guy@g1y.io>
2020-02-23 00:01:30 -0800

    Fix up unit tests for log parsing

--- log-scraper/main.py
+++ log-scraper/main.py
@@ -3,48 +3,72 @@ sentry_sdk.init("https://1ffbb5ff89b440338e3112310ac30750@sentry.io/1864498")
 
 import io
 import json
+import sys
+import logging
+logging.basicConfig()
+logger = logging.getLogger('Scraper')
 
 import pycurl
 from pymongo import MongoClient
 from kafka import KafkaProducer
 
-
 import slopd_log_parse
 
-buffer = io.BytesIO()
-c = pycurl.Curl()
-c.setopt(c.URL, 'https://pdreport.slocity.org/policelog/rpcdsum.txt')
-c.setopt(c.WRITEDATA, buffer)
-c.perform()
-c.close()
 
-body = buffer.getvalue().decode('iso-8859-1')
-print("Parsing logs")
-parsed = slopd_log_parse.parse_log(body)
-print("Logs parsed")
+class Scraper:
+	def __init__(self):
+		if 'unittest' not in sys.modules:
+			self.producer = KafkaProducer(bootstrap_servers='kafka-cluster:9092',
+					value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-print("Connecting to mongodb")
-client = MongoClient('mongodb')
-db = client.snoopy
-logs = db.police_logs
+			logger.info("Connecting to mongodb")
+			client = MongoClient('mongodb')
+			self.logs = client.snoopy.police_logs
 
-producer = KafkaProducer(bootstrap_servers='kafka-cluster:9092',
-		value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+		self.pulled_log = []
 
-totalInserted = 0
-totalEntries = 0
-for entry in parsed:
-	totalEntries += 1
-	existing = logs.find_one({"report_number": entry["report_number"]})
-	if not existing:
-		id = logs.insert_one(entry).inserted_id
-		print(f"{id} - inserting entry")
-		try:
-			producer.send('log-entries', {'id': id})
-			print(f"{id} - sent to kafka broker")
-		except Exception as e:
-			sentry_sdk.capture_exception(e)
+	def fetch_log(self):
+		buffer = io.BytesIO()
+		c = pycurl.Curl()
+		c.setopt(c.URL, 'https://pdreport.slocity.org/policelog/rpcdsum.txt')
+		c.setopt(c.WRITEDATA, buffer)
+		c.perform()
+		c.close()
+		body = buffer.getvalue().decode('iso-8859-1')
+		return body
 
-		totalInserted = totalInserted + 1
+	def parse_logs(self, log):
+		logger.info("Parsing logs")
+		parsed = slopd_log_parse.parse_log(log)
+		logger.info("Logs parsed")
 
-print(f"finished scraping. Created entries: {str(totalInserted)}. Total scraped: {str(totalEntries)}")
+		self.pulled_log = list(parsed)
+
+	def send_out_logs(self):
+		totalInserted = 0
+		totalEntries = 0
+		log = self.fetch_log()
+
+		self.parse_logs(log)
+		parsed = self.pulled_log
+
+		for entry in parsed:
+			totalEntries += 1
+			existing = self.logs.find_one({"report_number": entry["report_number"]})
+			if not existing:
+				id = self.logs.insert_one(entry).inserted_id
+				logger.info(f"{id} - inserting entry")
+				try:
+					#producer.send('log-entries', {'id': id})
+					logger.info(f"{id} - not sent to kafka broker")
+				except Exception as e:
+					sentry_sdk.capture_exception(e)
+
+				totalInserted = totalInserted + 1
+
+		logger.info(f"finished scraping. Created entries: {str(totalInserted)}. Total scraped: {str(totalEntries)}")
+
+
+if __name__ == "__main__":
+	scraper = Scraper()
+	scraper.send_out_logs()
--- log-scraper/slopd_log_parse.py
+++ log-scraper/slopd_log_parse.py
@@ -3,35 +3,34 @@ import pprint
 import datetime
 
 def parse_log(log_contents):
-	separator = '={79}\s\n'
-	expression = separator
-	lines = re.split(expression, log_contents)
+	separator = '={79}'
+	lines = re.split(separator, log_contents)
 	combined = combine_header_body(lines)
 	return map(parse_entry, combined)
 
+report_num_pattern          = re.compile("([0-9]+)\s")
+date_pattern                = re.compile("[0-9]+\s([0-9\/]+)\s")
+received_pattern            = re.compile("Received:([0-9]{2}:[0-9]{2})\s")
+dispatched_pattern          = re.compile("Dispatched:([0-9]{2}:[0-9]{2})\s")
+arrived_pattern             = re.compile("Arrived:([0-9]{2}:[0-9]{2})\s")
+cleared_pattern             = re.compile("Cleared:([0-9]{2}:[0-9]{2})\s")
+type_pattern                = re.compile("Type:\s*([a-zA-Z0-9]+)")
+location_pattern            = re.compile("Location:(\S*)")
+address_pattern             = re.compile("Addr: (.*)Clearance Code")
+grid_pattern                = re.compile("; GRID (.*)(,|;)")
+clearance_code_pattern      = re.compile("Clearance Code: (\S*)\s")
+responsible_officer_pattern = re.compile("Responsible Officer: (\S*, .)")
+call_comments_pattern       = re.compile("CALL COMMENTS: (.*)\n")
+description_pattern         = re.compile("Des:(.*)incid")
+
 def parse_entry(line):
 	entry = {'raw': line}
-
-	report_num_pattern          = re.compile("([0-9]+)\s")
-	date_pattern                = re.compile("[0-9]+\s([0-9\/]+)\s")
-	received_pattern            = re.compile("Received:([0-9]{2}:[0-9]{2})\s")
-	dispatched_pattern          = re.compile("Dispatched:([0-9]{2}:[0-9]{2})\s")
-	arrived_pattern             = re.compile("Arrived:([0-9]{2}:[0-9]{2})\s")
-	cleared_pattern             = re.compile("Cleared:([0-9]{2}:[0-9]{2})\s")
-	type_pattern                = re.compile("Type:\s*([a-zA-Z0-9]+)")
-	location_pattern            = re.compile("Location:(\S*)")
-	address_pattern             = re.compile("Addr: (.*)Clearance Code")
-	grid_pattern                = re.compile("; GRID (.*)(,|;)")
-	clearance_code_pattern      = re.compile("Clearance Code: (\S*)\s")
-	responsible_officer_pattern = re.compile("Responsible Officer: (\S*, .)")
-	call_comments_pattern       = re.compile("CALL COMMENTS: (.*)\n")
-	description_pattern         = re.compile("Des:(.*)incid")
 
 	match = report_num_pattern.match(line)
 	if match:
 		entry["report_number"] = match.group(1)
 
-	match = date_pattern.match(line)
+	match = date_pattern.search(line)
 	if match:
 		dateString = match.group(1)
 		entry["date"] = dateString
@@ -39,43 +38,41 @@ def parse_entry(line):
 		year = f"20{yearSmall}"
 		date = datetime.datetime(int(year), int(month), int(day))
 		timestamp = date.timestamp()
-		print(f"dateString: {dateString}, month, day, year: {month}/{day}/{year}, date: {date}, timestamp created: {timestamp}, timestamp cast: {int(timestamp)}")
 		entry["timestamp"] = int(timestamp)
 
-	search = received_pattern.search(line)
-	if search:
+	if search := received_pattern.search(line):
 		entry["received"] = search.group(1)
-	search = dispatched_pattern.search(line)
-	if search:
+
+	if search := dispatched_pattern.search(line):
 		entry["received"] = search.group(1)
-	search = arrived_pattern.search(line)
-	if search:
+
+	if search := arrived_pattern.search(line):
 		entry["arrived"] = search.group(1)
-	search = cleared_pattern.search(line)
-	if search:
+
+	if search := cleared_pattern.search(line):
 		entry["cleared"] = search.group(1)
-	search = type_pattern.search(line)
-	if search:
+
+	if search := type_pattern.search(line):
 		entry["type"] = search.group(1)
-	search = location_pattern.search(line)
-	if search:
+
+	if search := location_pattern.search(line):
 		entry["location"] = search.group(1)
-	search = address_pattern.search(line)
-	if search:
+
+	if search := address_pattern.search(line):
 		address_line = search.group(1)
 		address_sections = re.split(";", address_line)
 		entry["address"] = address_sections[0]
-	search = clearance_code_pattern.search(line)
-	if search:
+
+	if search := clearance_code_pattern.search(line):
 		entry["clearance_code"] = search.group(1)
-	search = responsible_officer_pattern.search(line)
-	if search:
+
+	if search := responsible_officer_pattern.search(line):
 		entry["responsible_officer"] = search.group(1)
-	search = call_comments_pattern.search(line)
-	if search:
+
+	if search := call_comments_pattern.search(line):
 		entry["call_comments"] = search.group(1)
-	search = description_pattern.search(line)
-	if search:
+
+	if search := description_pattern.search(line):
 		entry["description"] = search.group(1)
 
 	return entry
@@ -84,16 +81,16 @@ def parse_entry(line):
 # body and then recombining is how I decided to shortcut that.
 def combine_header_body(lines):
 	combined_lines = []
-	current_entry = ""
+	header = ""
+	body_pattern = re.compile('Type:\s')
+	header_pattern = re.compile(".*Received:.*Dispatched:")
+	total = 0
 	for line in lines:
-		body_pattern = re.compile("Type: (\S*)\s")
-		header_pattern = re.compile(".*Received:.*Dispatched:")
-		body_match = body_pattern.match(line)
-		header_match = header_pattern.match(line)
-		if body_match:
-			current_entry = current_entry + line
-			combined_lines.append(current_entry)
-		elif header_match:
-			current_entry = line
+		if body_match := body_pattern.search(line):
+			total += 1
+			event = header + line
+			combined_lines.append(event)
+		elif header_match := header_pattern.search(line):
+			header = line
 
 	return combined_lines
