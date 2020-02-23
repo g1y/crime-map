885b963662172e9bfc482a233f5e656ccf790eab
Guy Moore <guy@g1y.io>
2020-02-23 00:55:30 -0800

    Make a makefile also put regex's in raw strings

--- log-scraper/slopd_log_parse.py
+++ log-scraper/slopd_log_parse.py
@@ -8,22 +8,22 @@ def parse_log(log_contents):
 	combined = combine_header_body(lines)
 	return map(parse_entry, combined)
 
-address_pattern = re.compile("Addr: (.*)Clearance Code")
-date_pattern = re.compile("[0-9]+\s([0-9\/]+)\s")
+address_pattern = re.compile(r"Addr: (.*)Clearance Code")
+date_pattern = re.compile(r"[0-9]+\s([0-9\/]+)\s")
 
 simple_fields = {
-	'report_number': re.compile("([0-9]+)\s"),
-	'received': re.compile("Received:([0-9]{2}:[0-9]{2})\s"),
-	'dispatched': re.compile("Dispatched:([0-9]{2}:[0-9]{2})\s"),
-	'arrived': re.compile("Arrived:([0-9]{2}:[0-9]{2})\s"),
-	'cleared': re.compile("Cleared:([0-9]{2}:[0-9]{2})\s"),
-	'type': re.compile("Type:\s*([a-zA-Z0-9]+)"),
-	'location': re.compile("Location:(\S*)"),
-	'grid': re.compile("; GRID (.*)(,|;)"),
-	'clearance_code': re.compile("Clearance Code: (\S*)\s"),
-	'responsible_officer': re.compile("Responsible Officer: (\S*, .)"),
-	"call_comments": re.compile("CALL COMMENTS: (.*)\n"),
-	"description": re.compile("Des:(.*)incid"),
+	'report_number': re.compile(r"([0-9]+)\s"),
+	'received': re.compile(r"Received:([0-9]{2}:[0-9]{2})\s"),
+	'dispatched': re.compile(r"Dispatched:([0-9]{2}:[0-9]{2})\s"),
+	'arrived': re.compile(r"Arrived:([0-9]{2}:[0-9]{2})\s"),
+	'cleared': re.compile(r"Cleared:([0-9]{2}:[0-9]{2})\s"),
+	'type': re.compile(r"Type:\s*([a-zA-Z0-9]+)"),
+	'location': re.compile(r"Location:(\S*)"),
+	'grid': re.compile(r"; GRID (.*)(,|;)"),
+	'clearance_code': re.compile(r"Clearance Code: (\S*)\s"),
+	'responsible_officer': re.compile(r"Responsible Officer: (\S*, .)"),
+	"call_comments": re.compile(r"CALL COMMENTS: (.*)\n"),
+	"description": re.compile(r"Des:(.*)incid"),
 }
 
 def parse_entry(line):
@@ -56,8 +56,8 @@ def parse_entry(line):
 def combine_header_body(lines):
 	combined_lines = []
 	header = ""
-	body_pattern = re.compile('Type:\s')
-	header_pattern = re.compile(".*Received:.*Dispatched:")
+	body_pattern = re.compile(r'Type:\s')
+	header_pattern = re.compile(r".*Received:.*Dispatched:")
 	total = 0
 	for line in lines:
 		if body_match := body_pattern.search(line):
