902d2dc39f1eaf3b458e3803182ad1bbf174714a
Guy Moore <guy@g1y.io>
2020-02-25 01:52:18 -0800

    Update build script and add coverage deps

--- log-scraper/build.sh
+++ log-scraper/build.sh
@@ -2,10 +2,13 @@ docker build -t log-scraper .
 docker tag log-scraper g1ymoore/crime-map:log-scraper
 docker push g1ymoore/crime-map:log-scraper
 
-kubectl delete job test-log-scrape
-kubectl create job test-log-scrape --from cronjob/log-scraper
+kubectl delete job test-log-scraper
+kubectl get pods | grep test-log-scraper | awk '{print $1;}' | xargs kubectl delete pod
+kubectl create job test-log-scraper --from cronjob/log-scraper
 
-pod="$(kubectl get pods | grep test-log-scrape | awk '{print $1}')"
+pod=""
+while  [ -z "$pod" ]; do pod="$(kubectl get pods | grep test-log-scraper | awk '{print $1}')"; done
+
 echo "Waiting for pod ${pod} to be Ready"
 kubectl wait --for=condition=Ready "pod/${pod}"
 kubectl logs -f "${pod}"
