6fdd7dfbeae78250ed1aa151399bdf84d9ade1db
Ryan McDonough <Rhinomcd@gmail.com>
2021-06-27 12:45:42 -0700

    Convert tabs to spaces and reformat per PEP-8 guidelines

--- log-scraper/reparse_logs.py
+++ log-scraper/reparse_logs.py
@@ -1,30 +1,30 @@
+import sys
 import sentry_sdk
+import slopd_log_parse
+from pymongo import MongoClient
+
 sentry_sdk.init("https://1ffbb5ff89b440338e3112310ac30750@sentry.io/1864498")
 
-import sys
-import pprint
-
-from pymongo import MongoClient
-import slopd_log_parse
-
-
 if len(sys.argv) != 2:
-	print("Usage: reparse_Logs <field to update>")
-	sys.exit(1)
+    print("Usage: reparse_Logs <field to update>")
+    sys.exit(1)
+
 
 def reparse_entry(existing_entry):
-	reparsed = slopd_log_parse.parse_entry(existing_entry['raw'])
-	reparsed['_id'] = existing_entry['_id']
-	return reparsed
+    reparsed = slopd_log_parse.parse_entry(existing_entry['raw'])
+    reparsed['_id'] = existing_entry['_id']
+    return reparsed
+
 
 def reparse_all(field):
-	lookup = {}
-	entries = map(reparse_entry, logs.find(lookup))
-	for entry in entries:
-		id = entry[u'_id']
-		val = entry[field]
-		print(f"Updating: {id}, field: {field}, val: {val}")
-		existing = logs.update_one({'_id': entry[u'_id']}, {'$set': {field: entry[field]}})
+    lookup = {}
+    entries = map(reparse_entry, logs.find(lookup))
+    for entry in entries:
+        id = entry[u'_id']
+        val = entry[field]
+        print(f"Updating: {id}, field: {field}, val: {val}")
+        existing = logs.update_one({'_id': entry[u'_id']}, {'$set': {field: entry[field]}})
+
 
 client = MongoClient('mongodb', 27017)
 db = client.snoopy
--- log-scraper/slopd_log_parse.py
+++ log-scraper/slopd_log_parse.py
@@ -1,70 +1,72 @@
 import re
-import pprint
 import datetime
 
+
 def parse_log(log_contents):
-	separator = '={79}'
-	lines = re.split(separator, log_contents)
-	combined = combine_header_body(lines)
-	return map(parse_entry, combined)
+    separator = '={79}'
+    lines = re.split(separator, log_contents)
+    combined = combine_header_body(lines)
+    return map(parse_entry, combined)
+
 
 address_pattern = re.compile(r"Addr: (.*)Clearance Code")
 date_pattern = re.compile(r"[0-9]+\s([0-9\/]+)\s")
 
 simple_fields = {
-	'report_number': re.compile(r"([0-9]+)\s"),
-	'received': re.compile(r"Received:([0-9]{2}:[0-9]{2})\s"),
-	'dispatched': re.compile(r"Dispatched:([0-9]{2}:[0-9]{2})\s"),
-	'arrived': re.compile(r"Arrived:([0-9]{2}:[0-9]{2})\s"),
-	'cleared': re.compile(r"Cleared:([0-9]{2}:[0-9]{2})\s"),
-	'type': re.compile(r"Type:\s*([a-zA-Z0-9]+)"),
-	'location': re.compile(r"Location:(\S*)"),
-	'grid': re.compile(r"; GRID (.*)(,|;)"),
-	'clearance_code': re.compile(r"Clearance Code: (\S*)\s"),
-	'responsible_officer': re.compile(r"Responsible Officer: (\S*, .)"),
-	"call_comments": re.compile(r"CALL COMMENTS: (.*)\n"),
-	"description": re.compile(r"Des:(.*)incid"),
+    'report_number': re.compile(r"([0-9]+)\s"),
+    'received': re.compile(r"Received:([0-9]{2}:[0-9]{2})\s"),
+    'dispatched': re.compile(r"Dispatched:([0-9]{2}:[0-9]{2})\s"),
+    'arrived': re.compile(r"Arrived:([0-9]{2}:[0-9]{2})\s"),
+    'cleared': re.compile(r"Cleared:([0-9]{2}:[0-9]{2})\s"),
+    'type': re.compile(r"Type:\s*([a-zA-Z0-9]+)"),
+    'location': re.compile(r"Location:(\S*)"),
+    'grid': re.compile(r"; GRID (.*)(,|;)"),
+    'clearance_code': re.compile(r"Clearance Code: (\S*)\s"),
+    'responsible_officer': re.compile(r"Responsible Officer: (\S*, .)"),
+    "call_comments": re.compile(r"CALL COMMENTS: (.*)\n"),
+    "description": re.compile(r"Des:(.*)incid"),
 }
 
+
 def parse_entry(line):
-	entry = {'raw': line}
+    entry = {'raw': line}
 
-	if match := date_pattern.search(line):
-		dateString = match.group(1)
-		entry["date"] = dateString
-		month, day, yearSmall = re.split(r"\/", dateString)
-		year = f"20{yearSmall}"
-		date = datetime.datetime(int(year), int(month), int(day))
-		timestamp = date.timestamp()
-		entry["timestamp"] = int(timestamp)
+    if match := date_pattern.search(line):
+        dateString = match.group(1)
+        entry["date"] = dateString
+        month, day, yearSmall = re.split(r"\/", dateString)
+        year = f"20{yearSmall}"
+        date = datetime.datetime(int(year), int(month), int(day))
+        timestamp = date.timestamp()
+        entry["timestamp"] = int(timestamp)
 
-	if search := address_pattern.search(line):
-		address_line = search.group(1)
-		address_sections = re.split(";", address_line)
-		entry["address"] = address_sections[0]
+    if search := address_pattern.search(line):
+        address_line = search.group(1)
+        address_sections = re.split(";", address_line)
+        entry["address"] = address_sections[0]
+
+    for field_name in simple_fields:
+        regex = simple_fields[field_name]
+        if search := regex.search(line):
+            entry[field_name] = search.group(1)
+
+    return entry
 
 
-	for field_name in simple_fields:
-		regex = simple_fields[field_name]
-		if search := regex.search(line):
-			entry[field_name] = search.group(1)
-
-	return entry
-
-# It's hard to split on every nth occurence so splitting up the header and
+# It's hard to split on every nth occurrence so splitting up the header and
 # body and then recombining is how I decided to shortcut that.
 def combine_header_body(lines):
-	combined_lines = []
-	header = ""
-	body_pattern = re.compile(r'Type:\s')
-	header_pattern = re.compile(r".*Received:.*Dispatched:")
-	total = 0
-	for line in lines:
-		if body_match := body_pattern.search(line):
-			total += 1
-			event = header + line
-			combined_lines.append(event)
-		elif header_match := header_pattern.search(line):
-			header = line
+    combined_lines = []
+    header = ""
+    body_pattern = re.compile(r'Type:\s')
+    header_pattern = re.compile(r".*Received:.*Dispatched:")
+    total = 0
+    for line in lines:
+        if body_match := body_pattern.search(line):
+            total += 1
+            event = header + line
+            combined_lines.append(event)
+        elif header_match := header_pattern.search(line):
+            header = line
 
-	return combined_lines
+    return combined_lines
